294a9097b2384e0006ce5745f18c29962b96ef23
GloryUsen <ganwana89@gmail.com>
2026-01-20 13:26:19 +0100

    Nothing to commit yet

---
+++ src/main/java/com/glory/Simple/RestAPI/For/BankingApp/SimpleRestApiForBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.glory.Simple.RestAPI.For.BankingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestApiForBankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestApiForBankingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glory/Simple/RestAPI/For/BankingApp/SimpleRestApiForBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glory.Simple.RestAPI.For.BankingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestApiForBankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
